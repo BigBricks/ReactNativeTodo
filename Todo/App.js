93ba260cdf010e821b3a787f5256c2fa3726268b
BigBricks <BSA6.23.94@gmail.com>
2018-11-02 15:20:32 -0400

    learning about views and how they interact

--- Todo/App.js
+++ Todo/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { white } from "ansi-colors";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image source={require('./assets/morocco.jpg')} />
+        <ImageBackground source={require('./assets/morocco.jpg')} style={{ width: '100%', height: '100%' }} >
+          <View style={styles1.container}>
+            <Text style={{
+              color: 'white'
+            }}>BOB</Text></View>
+        </ImageBackground>
         <Text>BOB</Text>
       </View>
     );
@@ -16,6 +22,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center"
   }
