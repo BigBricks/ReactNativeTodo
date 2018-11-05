69129c6aee502e2a0dafd1dfdac37e04f5aeb491
BigBricks <BSA6.23.94@gmail.com>
2018-11-05 16:42:06 -0500

    data is showing up just need to research some css styling

--- Todo/App.js
+++ Todo/App.js
@@ -1,23 +1,36 @@
 import React from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import { white } from "ansi-colors";
+import { FlatList } from "react-native-gesture-handler";
 
 export default class App extends React.Component {
   state = {
-    todo: [],
+    todo: [{ text: 'hi11111111111111' }, { text: 'hi11111111111111' }],
     text: ""
   };
+  handleTextInput = text => {
+    this.setState({ text: text })
+  }
   render() {
     return (
       <View style={styles.container}>
         <ImageBackground source={require('./assets/morocco.jpg')} style={{ width: '100%', height: '100%' }} >
-          <View style={styles1.container}>
-            <Text style={{
-              color: 'white'
-            }}>BOB</Text></View>
+          <FlatList
+            data={this.state.todo}
+            renderItem={({ item, index }) =>
+              <View style={styles1.container}>
+                <Text style={{
+                  color: 'pink'
+                }}>
+                  {item.text}
+                </Text>
+                {/* <Button title="X" onPress={() => this.deleteTask(index)} /> */}
+              </View>}
+            keyExtractor={(item, index) => index.toString()}
+          />
         </ImageBackground>
         <Text>BOB</Text>
-      </View>
+      </View >
     );
   }
 }
