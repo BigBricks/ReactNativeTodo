cb825969ab56d0614a089808d70b0debd74c2cf3
BigBricks <BSA6.23.94@gmail.com>
2018-11-15 18:22:30 -0500

    added to the state to do some more styling tests

--- Todo/App.js
+++ Todo/App.js
@@ -5,40 +5,53 @@ import { FlatList } from "react-native-gesture-handler";
 
 export default class App extends React.Component {
   state = {
-    todo: [{ text: 'hi11111111111111' }, { text: 'hi11111111111111' }],
+    todo: [
+      { text: "hi11111111111111" },
+      { text: "hi11111111111111" },
+      { text: "hi11111111111111" },
+      { text: "hi11111111111111" },
+      { text: "hi11111111111111" },
+      { text: "hi11111111111111" }
+    ],
     text: ""
   };
   handleTextInput = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
   addTodo() {
     const { text } = this.state;
     this.state.todo.push(text);
-    this.setState({ text: "" })
+    this.setState({ text: "" });
   }
   render() {
     return (
       <View style={styles.container}>
-        <ImageBackground source={require('./assets/morocco.jpg')} style={{ width: '100%', height: '100%' }} >
+        <ImageBackground
+          source={require("./assets/morocco.jpg")}
+          style={{ width: "100%", height: "100%" }}
+        >
           <View style={styles1.container}>
             <FlatList
               data={this.state.todo}
-              renderItem={({ item, index }) =>
+              renderItem={({ item, index }) => (
                 <View>
                   <View>
-                    <Text style={{
-                      color: 'pink'
-                    }}>
+                    <Text
+                      style={{
+                        color: "pink"
+                      }}
+                    >
                       {item.text}
                     </Text>
                   </View>
-                </View>}
+                </View>
+              )}
               keyExtractor={(item, index) => index.toString()}
             />
           </View>
         </ImageBackground>
         <Text>BOB</Text>
-      </View >
+      </View>
     );
   }
 }
